8a50ad299f9ed509de5435f45287f15fc46f5e88
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-01-30 14:34:49 +1300

    Initial Commit

---
+++ AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UnitConverter
+//
+//  Created by Mel John del Barrio on 30/01/17.
+//  Copyright © 2017 Mel John del Barrio. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UnitConverter
+//
+//  Created by Mel John del Barrio on 30/01/17.
+//  Copyright © 2017 Mel John del Barrio. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
